e7c5534b4f1747034eda0cbcf361312e5bb46e68
Moaz Ali <151751659+40moaz@users.noreply.github.com>
2025-06-30 08:10:43 +0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,26 @@
+// api/index.js
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("../config/db");
+
+dotenv.config();
+
+const app = express();
+
+// DB Connection
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API is running from Vercel Serverless!");
+});
+
+app.use("/api/auth", require("../routes/auth"));
+
+// Export as handler
+module.exports.handler = serverless(app);
