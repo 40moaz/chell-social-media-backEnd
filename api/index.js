d5fd5a4f71608d68c11d0cc221dde05d71321d6b
Moaz Ali <151751659+40moaz@users.noreply.github.com>
2025-07-21 17:20:45 +0300

    remove notification messages from app

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("../config/db");
-const http = require("http");+2
+const http = require("http");
 const WebSocket = require("ws");
 const { clients } = require("../ws/clients");
 
